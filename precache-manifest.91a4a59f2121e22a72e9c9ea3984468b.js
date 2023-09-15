dd7b1ce6f93a517caf7cb35cb47c49f1038e0ccd
Abdullah Riaz <ariaz988@gmail.com>
2023-09-15 13:31:31 -0700

    Updates

---
+++ precache-manifest.91a4a59f2121e22a72e9c9ea3984468b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8d41f785358c1fd8391",
+    "url": "/static/css/main.cb070bad.chunk.css"
+  },
+  {
+    "revision": "d8d41f785358c1fd8391",
+    "url": "/static/js/main.59434a02.chunk.js"
+  },
+  {
+    "revision": "c2a4eb1306cf1afe55d9",
+    "url": "/static/js/runtime~main.f7f764ff.js"
+  },
+  {
+    "revision": "15f1a338604c14122b34",
+    "url": "/static/js/2.ff531a51.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "f01ad4713b72350177c66d347297ea3a",
+    "url": "/static/media/linkedinphoto.f01ad471.jpeg"
+  },
+  {
+    "revision": "4fd5df84c35f40fc72b0c16e4a522d86",
+    "url": "/static/media/picture.4fd5df84.PNG"
+  },
+  {
+    "revision": "93125176c552d603fab331f2a43cb35a",
+    "url": "/index.html"
+  }
+];
